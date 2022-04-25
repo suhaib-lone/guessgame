318a894b1230c95e5bac3d3094c8dba2df36b626
suhaib-lone <suhaibsareer09@gmail.com>
2022-04-25 15:16:01 +0530

    fixed bugs

--- game.js
+++ game.js
@@ -9,78 +9,90 @@ let Guessed=document.querySelector('#Guessed');
 let Status=document.querySelector('#status');
 let guessNum=Math.floor(Math.random()*100 +1);
 let guessCount=1;
+let resetbtn;
 
 update.innerHTML ="Guess a number between 1 and 100";
 
-//ON SUBMIT EVENT LISTNER
+function lowORhi(){
 
-enter.addEventListener('click', function(){
+}
 
-    if(gnum.value<1 || gnum.value>100){
-        update.innerHTML="GUESS CORRECT NUMS";
+function checkGuess(){
+    const guesses=Number(gnum.value);
+    if(guessCount===1){
+        Guessed.textContent="Previous Guess: ";
     }
-    else if(guessCount>10){
-        Status.innerHTML ="YOU LOSE!";
-        Status.style.backgroundColor ="pink";
-        Status.style.color="red";
+    preNums.textContent+=guesses + ' ';
+
+    if(guessCount>10){
+        Status.innerHTML ='YOU LOSE!';
+        Status.style.backgroundColor ='pink';
+        Status.style.color='red';
         update.innerHTML="";
-        gnum.disabled =true;
-        enter.disabled =true;
         reSET();
     }
+    else if(gnum.value<1 || gnum.value>100){
+        update.innerHTML="GUESS CORRECT NUMS";
+    }
+
     else if(gnum.value ==guessNum){
-        Status.innerHTML ="YOU WON!";
+        Status.innerHTML ='YOU WON!';
+        Status.style.color='yellow';
         update.innerHTML="";
-        Status.style.backgroundColor ="green";
-        Status.style.color="yellow";
-        gnum.disabled =true;
-        enter.disabled =true;
+        Status.style.backgroundColor ='green';
         reSET();
     }
     else{
+        Status.textContent ='wrong';
+        Status.style.color='red';
+        Status.style.backgroundColor ='pink';
+
         if(gnum.value >guessNum){
             update.innerHTML ="Number too high!";
         }
         else{
             update.innerHTML ="Number too low!";
         }
-        Status.innerHTML ="wrong";
-        Status.style.color="red";
-        Status.style.backgroundColor ="pink";
-        let savePreNums=[];
-        savePreNums+= gnum.value;
-        Guessed.textContent="Previous Guess:";
-        preNums.textContent+=`${savePreNums} `;
     }
-    
+
     guessCount++;
     gnum.value="";
     gnum.focus();
-})
+}
+
+
+
+//ON SUBMIT EVENT LISTNER
+
+enter.addEventListener('click', checkGuess);
 
 //RESET FUNCTION TO RESET THE GAME
 
 function reSET(){
+    gnum.disabled =true;
+    enter.disabled =true;
     resetbtn=document.createElement('button');
     resetbtn.textContent ='Reset Game';
     resetbtn.style="background-color:black;padding:5px;border-radius:2px;font-size:15px;color:cyan";
     guessbox.appendChild(resetbtn);
-    resetbtn.addEventListener('click',newGame)
+    resetbtn.addEventListener('click',newGame);
 }
 
 //ON RESET EVENT LISTNER
 
 function newGame(){
+    guessCount=1;
     update.innerHTML ="Guess a number between 1 and 100";
-    gnum.value="";
     Guessed.textContent='';
-    preNums.textContent='';
-    Status.innerHTML ="";
-    Status.style.display="none";
+    const resetParas = document.querySelectorAll('p');
+    for (const resetPara of resetParas) {
+      resetPara.textContent = '';
+    }
+    gnum.value='';
     gnum.disabled = false;
     enter.disabled =false;
-    guessCount=1;
+    resetbtn.parentNode.removeChild(resetbtn);
+    Status.style.background='none';
     gnum.focus();
     guessNum=Math.floor(Math.random()*100 +1);
-    resetbtn.parentNode.removeChild(resetbtn);
 }
